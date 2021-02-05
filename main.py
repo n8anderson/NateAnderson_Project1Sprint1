7628b3372c4b8e73ab73245eb682ed9ff86c7e1e
Nate Anderson <n8anderson@student.bridgew.edu>
2021-02-04 18:40:41 -0600

    Added comments and style guide corrections

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import secrets
 
-def get_data(url:str,page):
+
+# Actually gets the data from one page and saves it to an array
+def get_data(url: str, page):
     all_data = []
     full_url = f"{url}&api_key={secrets.api_key}&page={page}"
     response = requests.get(full_url)
@@ -13,10 +15,15 @@ def get_data(url:str,page):
     all_data.extend(results)
     return all_data
 
+
+# Main function that saves data from the website into a .txt file
 def main():
     f = open("School_Data.txt", "w")
     for page in range(1, 161):
-        url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
+        url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2," \
+              "3&fields=id,school.state,school.name,school.city,2018.student.size," \
+              "2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line," \
+              "2016.repayment.3_yr_repayment.overall"
         all_data = get_data(url,page)
         for item in all_data:
             f.write(str(item))
@@ -25,7 +32,8 @@ def main():
     print("File Completed")
     f.close()
 
-#If running to get functions dont run main
-#If running to run, run main
+
+# If running to get functions dont run main
+# If running to run, run main
 if __name__ == '__main__':
     main()
