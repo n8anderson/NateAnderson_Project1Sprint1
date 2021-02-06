039085ca0cd90e24fa33af2891fe19dd00dd439f
Nate Anderson <n8anderson@student.bridgew.edu>
2021-02-06 09:55:18 -0600

    Getting page numbers from metadata instead of hard coding the number in. Allows for room to update webpage without breaking program.

--- main.py
+++ main.py
@@ -3,33 +3,48 @@ import secrets
 
 
 # Actually gets the data from one page and saves it to an array
-def get_data(url: str, page):
+def get_data(url: str):
     all_data = []
-    full_url = f"{url}&api_key={secrets.api_key}&page={page}"
-    response = requests.get(full_url)
-    if response.status_code != 200:
-        print(response.text)
-        return []
-    json_data = response.json()
-    results = json_data['results']
-    all_data.extend(results)
+    page = 0
+    another_page = True
+    print("Loading data from website: ")
+    while another_page:
+        full_url = f"{url}&api_key={secrets.api_key}&page={page}"
+        response = requests.get(full_url)
+        if response.status_code != 200:
+            print(response.text)
+            return []
+        json_data = response.json()
+        results = json_data['results']
+        all_data.extend(results)
+        another_page = next_page(page, int(json_data["metadata"]["total"])//int(json_data["metadata"]["per_page"]))
+        if another_page:
+            page += 1
+    print("Finished Loading.")
     return all_data
+
+
+def next_page(page, total_page):
+    if page == total_page:
+        return False
+    else:
+        print("{:.2f}".format(page/total_page * 100), "%")
+        return True
 
 
 # Main function that saves data from the website into a .txt file
 def main():
     f = open("School_Data.txt", "w")
-    for page in range(0, 161):
-        url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2," \
-              "3&fields=id,school.state,school.name,school.city,2018.student.size," \
-              "2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line," \
-              "2016.repayment.3_yr_repayment.overall"
-        all_data = get_data(url, page)
-        for item in all_data:
-            f.write(str(item))
-            f.write("\n")
-        print("{:.2f}".format(page/161 * 100), "%")
-    print("File Completed")
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2," \
+          "3&fields=id,school.state,school.name,school.city,2018.student.size," \
+          "2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line," \
+          "2016.repayment.3_yr_repayment.overall"
+    all_data = get_data(url)
+    print("Writing File.")
+    for item in all_data:
+        f.write(str(item))
+        f.write("\n")
+    print("File Completed.")
     f.close()
 
 
